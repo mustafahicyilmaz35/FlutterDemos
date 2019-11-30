b31a4ecee434341b6590b883fdb4a6193e9a99d0
mustafahicyilmaz35 <mustafahicyilmaz35@gmail.com>
2019-12-01 00:08:52 +0300

    initial

--- shop/lib/screens/home.dart
+++ shop/lib/screens/home.dart
@@ -16,29 +16,30 @@ class Home extends StatelessWidget {
                     Text(
                       "ASUS Laptop",
                       textDirection: TextDirection.ltr,
-                      style: TextStyle(fontSize: 22.0),
+                      style: TextStyle(fontSize: 22.0, color: Colors.white, decoration: TextDecoration.none),
                     ),
                     Expanded(
                       child: Text(
                         " 16 GB RAM 128 GB SSD İntel i7",
                         textDirection: TextDirection.ltr,
-                        style: TextStyle(fontSize: 22.0),
+                        style: TextStyle(fontSize: 22.0,decoration: TextDecoration.none),
                       ),
                     )
                   ],
-                ),Row(
+                ),
+                Row(
                   textDirection: TextDirection.ltr,
                   children: <Widget>[
                     Text(
                       "HP Laptop",
                       textDirection: TextDirection.ltr,
-                      style: TextStyle(fontSize: 22.0),
+                      style: TextStyle(fontSize: 22.0,decoration: TextDecoration.none),
                     ),
                     Expanded(
                       child: Text(
                         " 32 GB RAM 512 GB SSD İntel i7",
                         textDirection: TextDirection.ltr,
-                        style: TextStyle(fontSize: 22.0),
+                        style: TextStyle(fontSize: 22.0,decoration: TextDecoration.none),
                       ),
                     )
                   ],
@@ -47,23 +48,34 @@ class Home extends StatelessWidget {
                   child: OrderButton(),
                   textDirection: TextDirection.ltr,
                 )
-                
               ],
             )));
   }
 }
-class OrderButton extends StatelessWidget{
+
+class OrderButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var button = Container(
-      margin: EdgeInsets.only(top:50.0),
+      margin: EdgeInsets.only(top: 50.0),
       child: RaisedButton(
         elevation: 5.0,
         child: Text("Go to check out"),
-        onPressed: (){},
+        onPressed: () {
+          order(context);
+        },
       ),
     );
     return button;
   }
 
+  order(BuildContext context) {
+    var alert = AlertDialog(
+      title: Text(
+        "Go to checkout",
+      ),
+      content: Text("I will go to check out"),
+    );
+    showDialog(context: context, builder: (BuildContext context) => alert);
+  }
 }
